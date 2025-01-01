e361828abc8483900d377aafbe3a76cc3f4fcfb5
Marmik <marmikpatel246@gmail.com>
2025-01-01 12:13:57 +0530

    Left

---
+++ OOPS/Polymorphism/CVRT,Runtime,Dynamic Binding/Animal_Sound.java
@@ -0,0 +1,11 @@
+// Scenario 1: Animal Sounds
+// In an animal sounds application, create a base class called "Animal" with a method called "makeSound" that returns an instance of the "Sound" class. Implement derived classes like "Dog" and "Cat" that override the "makeSound" method and return their specific sound objects.
+
+class Animal{
+    
+}
+public class Animal_Sound {
+    public static void main(String[] args) {
+        
+    }
+}
